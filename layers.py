f59a00f88087950ca8b206584f1870e65e49a72c
randomvisions <rkhozouei@hotmail.com>
2016-08-31 14:07:08 +1000

    Update layers.py

--- layers.py
+++ layers.py
@@ -1,232 +1,121 @@
 import numpy as np
 
-my_code = 0
-
-if my_code == 1:
-
-	def affine_forward(x, w, b):
-		"""
-		Computes the forward pass for an affine (fully-connected) layer.
-
-		The input x has shape (N, d_1, ..., d_k) and contains a minibatch of N
-		examples, where each example x[i] has shape (d_1, ..., d_k). We will
-		reshape each input into a vector of dimension D = d_1 * ... * d_k, and
-		then transform it to an output vector of dimension M.
-
-		Inputs:
-		- x: A numpy array containing input data, of shape (N, d_1, ..., d_k)
-		- w: A numpy array of weights, of shape (D, M)
-		- b: A numpy array of biases, of shape (M,)
-
-		Returns a tuple of:
-		- out: output, of shape (N, M)
-		- cache: (x, w, b)
-		"""
-		out = None
-		#############################################################################
-		# TODO: Implement the affine forward pass. Store the result in out. You     #
-		# will need to reshape the input into rows.                                 #
-		#############################################################################
-
-		# Fully connected output (first reshape the multidimensional input to match w dimensions)
-		out = np.dot(x.reshape(x.shape[0],-1), w) + b
-
-		#############################################################################
-		#                             END OF YOUR CODE                              #
-		#############################################################################
-		cache = (x, w, b)
-		return out, cache
+def affine_forward(x, w, b):
+	"""
+	Computes the forward pass for an affine (fully-connected) layer.
+	
+	The input x has shape (N, d_1, ..., d_k) and contains a minibatch of N
+	examples, where each example x[i] has shape (d_1, ..., d_k). We will
+	reshape each input into a vector of dimension D = d_1 * ... * d_k, and
+	then transform it to an output vector of dimension M.
+	
+	Inputs:
+	- x: A numpy array containing input data, of shape (N, d_1, ..., d_k)
+	- w: A numpy array of weights, of shape (D, M)
+	- b: A numpy array of biases, of shape (M,)
+	
+	Returns a tuple of:
+	- out: output, of shape (N, M)
+	- cache: (x, w, b)
+	"""
+	out = None
+	#############################################################################
+	# TODO: Implement the affine forward pass. Store the result in out. You     #
+	# will need to reshape the input into rows.                                 #
+	#############################################################################
+	
+	# Fully connected output (first reshape the multidimensional input to match w dimensions)
+	out = np.dot(x.reshape(x.shape[0],-1), w) + b
+	
+	#############################################################################
+	#                             END OF YOUR CODE                              #
+	#############################################################################
+	cache = (x, w, b)
+	return out, cache
 
 
-	def affine_backward(dout, cache):
-		"""
-		Computes the backward pass for an affine layer.
+def affine_backward(dout, cache):
+	"""
+	Computes the backward pass for an affine layer.
+	
+	Inputs:
+	- dout: Upstream derivative, of shape (N, M)
+	- cache: Tuple of:
+	- x: Input data, of shape (N, d_1, ... d_k)
+	- w: Weights, of shape (D, M)
+	
+	Returns a tuple of:
+	- dx: Gradient with respect to x, of shape (N, d1, ..., d_k)
+	- dw: Gradient with respect to w, of shape (D, M)
+	- db: Gradient with respect to b, of shape (M,)
+	"""
+	x, w, b = cache
+	dx, dw, db = None, None, None
+	#############################################################################
+	# TODO: Implement the affine backward pass.                                 #
+	#############################################################################
+	dx = np.dot(w, dout.T).T.reshape(x.shape)
+	
+	dw = np.dot(x.reshape(x.shape[0],-1).T, dout)
+	
+	db = np.sum(dout, axis = 0)
+	
+	#############################################################################
+	#                             END OF YOUR CODE                              #
+	#############################################################################
+	return dx, dw, db
 
-		Inputs:
-		- dout: Upstream derivative, of shape (N, M)
-		- cache: Tuple of:
-		- x: Input data, of shape (N, d_1, ... d_k)
-		- w: Weights, of shape (D, M)
+def relu_forward(x):
+  """
+  Computes the forward pass for a layer of rectified linear units (ReLUs).
 
-		Returns a tuple of:
-		- dx: Gradient with respect to x, of shape (N, d1, ..., d_k)
-		- dw: Gradient with respect to w, of shape (D, M)
-		- db: Gradient with respect to b, of shape (M,)
-		"""
-		x, w, b = cache
-		dx, dw, db = None, None, None
-		#############################################################################
-		# TODO: Implement the affine backward pass.                                 #
-		#############################################################################
-		dx = np.dot(w, dout.T).T.reshape(x.shape)
+  Input:
+  - x: Inputs, of any shape
 
-		dw = np.dot(x.reshape(x.shape[0],-1).T, dout)
-
-		db = np.sum(dout, axis = 0)
-
-		#############################################################################
-		#                             END OF YOUR CODE                              #
-		#############################################################################
-		return dx, dw, db
-
-	def relu_forward(x):
-		  """
-		  Computes the forward pass for a layer of rectified linear units (ReLUs).
-
-		  Input:
-		  - x: Inputs, of any shape
-
-		  Returns a tuple of:
-		  - out: Output, of the same shape as x
-		  - cache: x
-		  """
-		  out = None
-		  #############################################################################
-		  # TODO: Implement the ReLU forward pass.                                    #
-		  #############################################################################
-		  
-		  # Rectified Linear Unit forward pass is threshold.
-		  out = np.maximum(x,0)  
-		  
-		  #############################################################################
-		  #                             END OF YOUR CODE                              #
-		  #############################################################################
-		  cache = x
-		  return out, cache
-
-
-	def relu_backward(dout, cache):
-		  """
-		  Computes the backward pass for a layer of rectified linear units (ReLUs).
-
-		  Input:
-		  - dout: Upstream derivatives, of any shape
-		  - cache: Input x, of same shape as dout
-
-		  Returns:
-		  - dx: Gradient with respect to x
-		  """
-		  dx, x = None, cache
-		  #############################################################################
-		  # TODO: Implement the ReLU backward pass.                                   #
-		  #############################################################################
-
-		  #ReLu backward pass is thresholding of upstream gradient. 
-		  dout[x<=0] = 0
-		  
-		  dx = dout
-		  
-		  #############################################################################
-		  #                             END OF YOUR CODE                              #
-		  #############################################################################
-		  return dx
-else:  # NOT MY CODE START****NOT MY CODE START****NOT MY CODE START****NOT MY CODE START****NOT MY CODE START****
-	def affine_forward(x, w, b):
-	  """
-	  Computes the forward pass for an affine (fully-connected) layer.
-	  The input x has shape (N, d_1, ..., d_k) where x[i] is the ith input.
-	  We multiply this against a weight matrix of shape (D, M) where
-	  D = \prod_i d_i
-	  Inputs:
-	  x - Input data, of shape (N, d_1, ..., d_k)
-	  w - Weights, of shape (D, M)
-	  b - Biases, of shape (M,)
-	  
-	  Returns a tuple of:
-	  - out: output, of shape (N, M)
-	  - cache: (x, w, b)
-	  """
-	  out = None
-	  #############################################################################
-	  # TODO: Implement the affine forward pass. Store the result in out. You     #
-	  # will need to reshape the input into rows.                                 #
-	  #############################################################################
-	  N = x.shape[0]
-	  D = np.prod(x.shape[1:])
-	  x2 = np.reshape(x, (N, D))
-	  out = np.dot(x2, w) + b
-	  #############################################################################
-	  #                             END OF YOUR CODE                              #
-	  #############################################################################
-	  cache = (x, w, b)
-	  return out, cache
+  Returns a tuple of:
+  - out: Output, of the same shape as x
+  - cache: x
+  """
+  out = None
+  #############################################################################
+  # TODO: Implement the ReLU forward pass.                                    #
+  #############################################################################
+  
+  # Rectified Linear Unit forward pass is threshold.
+  out = np.maximum(x,0)  
+  
+  #############################################################################
+  #                             END OF YOUR CODE                              #
+  #############################################################################
+  cache = x
+  return out, cache
 
 
-	def affine_backward(dout, cache):
-	  """
-	  Computes the backward pass for an affine layer.
-	  Inputs:
-	  - dout: Upstream derivative, of shape (N, M)
-	  - cache: Tuple of:
-		- x: Input data, of shape (N, d_1, ... d_k)
-		- w: Weights, of shape (D, M)
-	  Returns a tuple of:
-	  - dx: Gradient with respect to x, of shape (N, d1, ..., d_k)
-	  - dw: Gradient with respect to w, of shape (D, M)
-	  - db: Gradient with respect to b, of shape (M,)
-	  """
-	  x, w, b = cache
-	  dx, dw, db = None, None, None
-	  #############################################################################
-	  # TODO: Implement the affine backward pass.                                 #
-	  #############################################################################
-	  N = x.shape[0]
-	  D = np.prod(x.shape[1:])
-	  x2 = np.reshape(x, (N, D))
+def relu_backward(dout, cache):
+  """
+  Computes the backward pass for a layer of rectified linear units (ReLUs).
 
-	  dx2 = np.dot(dout, w.T) # N x D
-	  dw = np.dot(x2.T, dout) # D x M
-	  db = np.dot(dout.T, np.ones(N)) # M x 1
+  Input:
+  - dout: Upstream derivatives, of any shape
+  - cache: Input x, of same shape as dout
 
-	  dx = np.reshape(dx2, x.shape)
-	  #############################################################################
-	  #                             END OF YOUR CODE                              #
-	  #############################################################################
-	  return dx, dw, db
+  Returns:
+  - dx: Gradient with respect to x
+  """
+  dx, x = None, cache
+  #############################################################################
+  # TODO: Implement the ReLU backward pass.                                   #
+  #############################################################################
 
-
-	def relu_forward(x):
-	  """
-	  Computes the forward pass for a layer of rectified linear units (ReLUs).
-	  Input:
-	  - x: Inputs, of any shape
-	  Returns a tuple of:
-	  - out: Output, of the same shape as x
-	  - cache: x
-	  """
-	  out = None
-	  #############################################################################
-	  # TODO: Implement the ReLU forward pass.                                    #
-	  #############################################################################
-	  out = np.maximum(0, x)
-	  #############################################################################
-	  #                             END OF YOUR CODE                              #
-	  #############################################################################
-	  cache = x
-	  return out, cache
-
-
-	def relu_backward(dout, cache):
-	  """
-	  Computes the backward pass for a layer of rectified linear units (ReLUs).
-	  Input:
-	  - dout: Upstream derivatives, of any shape
-	  - cache: Input x, of same shape as dout
-	  Returns:
-	  - dx: Gradient with respect to x
-	  """
-	  dx, x = None, cache
-	  #############################################################################
-	  # TODO: Implement the ReLU backward pass.                                   #
-	  #############################################################################
-	  dx = np.array(dout, copy=True)
-	  dx[x <= 0] = 0
-	  #############################################################################
-	  #                             END OF YOUR CODE                              #
-	  #############################################################################
-	  return dx
-# NOT MY CODE END****NOT MY CODE END****NOT MY CODE END****NOT MY CODE END****NOT MY CODE END****NOT MY CODE END****
-
+  #ReLu backward pass is thresholding of upstream gradient. 
+  dout[x<=0] = 0
+  
+  dx = dout
+  
+  #############################################################################
+  #                             END OF YOUR CODE                              #
+  #############################################################################
+  return dx
 
 def batchnorm_forward(x, gamma, beta, bn_param):
   """
